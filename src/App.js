653c2c5f11079781c8c85799ee3d32722cdb82c2
j1powder <93947216+j1powder@users.noreply.github.com>
2022-08-09 15:42:09 -0700

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import ExpenseDisplay from "./ExpenseDisplay";
+
+function App() {
+
+
+  return (
+    <div className="expenses">
+      <h2>Let's get started!</h2>
+      <p>This is also Visible!</p>
+      <ExpenseDisplay></ExpenseDisplay>
+
+    </div>
+  );
+}
+
+export default App;
