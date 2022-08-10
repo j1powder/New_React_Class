6c9c817448ef298f35e8393114ec623dca4d6fd2
j1powder <93947216+j1powder@users.noreply.github.com>
2022-08-10 06:03:31 -0700

    Add files via upload

--- src/ExpenseDisplay.js
+++ src/ExpenseDisplay.js
@@ -1,32 +1,34 @@
 import ExpenseItem from "./ExpenseItem";
+import Card from './Card';
 import './ExpenseDisplay.css';
 
+
 function ExpenseDisplay(){
-    const expenses = [
-        {
-          id: 'e1',
-          title: 'Toilet Paper',
-          amount: 94.12,
-          date: new Date(2020, 7, 14),
-        },
-        { id: 'e2', title: 'New TV', amount: 799.49, date: new Date(2021, 2, 12) },
-        {
-          id: 'e3',
-          title: 'Car Insurance',
-          amount: 294.67,
-          date: new Date(2021, 2, 28),
-        },
-        {
-          id: 'e4',
-          title: 'New Desk (Wooden)',
-          amount: 450,
-          date: new Date(2021, 5, 12),
-        },
-    
-      ];
+  const expenses = [
+    {
+      id: 'e1',
+      title: 'Toilet Paper',
+      amount: 94.12,
+      date: new Date(2020, 7, 14),
+    },
+    { id: 'e2', title: 'New TV', amount: 799.49, date: new Date(2021, 2, 12) },
+    {
+      id: 'e3',
+      title: 'Car Insurance',
+      amount: 294.67,
+      date: new Date(2021, 2, 28),
+    },
+    {
+      id: 'e4',
+      title: 'New Desk (Wooden)',
+      amount: 450,
+      date: new Date(2021, 5, 12),
+    },
+
+  ];
 
     return (
-        <div><ExpenseItem 
+        <Card className="expenses"><ExpenseItem 
         title={expenses[0].title} 
         amount={expenses[0].amount} 
         date={expenses[0].date}
@@ -46,7 +48,7 @@ function ExpenseDisplay(){
                     amount={expenses[3].amount} 
                     date={expenses[3].date}
         ></ExpenseItem> 
-        </div>
+        </Card>
            );
 }
 
